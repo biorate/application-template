d67c90c91b006f72298754f104f3663bfb2227af
Leonid Levkin <llevkin@yandex.ru>
2023-11-30 17:13:31 +0300

    feat: @biorate/mocha added

--- apps/server/tests/001.example.spec.ts
+++ apps/server/tests/001.example.spec.ts
@@ -1,18 +1,22 @@
 import { container } from '@biorate/inversion';
+import { suite, test, timeout } from '@biorate/mocha';
+import { Done } from 'mocha';
 import { Root } from './__mocks__';
 
-describe('Example', function () {
-  this.timeout(3e4);
-
-  before(async () => {
+@suite('Example')
+@timeout(4000)
+class Example {
+  protected static async before() {
     await container.get(Root).$run();
-  });
+  }
 
-  it('/probe/readiness (GET)', (done) => {
+  @test('/probe/readiness (GET)')
+  protected probeReadiness(done: Done) {
     Root.supertest.get('/probe/readiness').expect(200, done);
-  });
+  }
 
-  it('/probe/healthz (GET)', (done) => {
+  @test('/probe/healthz (GET)')
+  protected probeHealthz(done: Done) {
     Root.supertest.get('/probe/healthz').expect(200, done);
-  });
-});
+  }
+}
